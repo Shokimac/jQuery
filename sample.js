1be734273aac22f84fa89755c74838cb36be0909
Shoki Yokomizo <curin030204@yahoo.co.jp>
2021-02-14 14:42:53 +0900

    add_comment

--- sample.js
+++ sample.js
@@ -1,18 +1,24 @@
 window.onload = function () {
     $('#send-form').submit(function () {
         
+        // 各フォームの値を取得する
         var name = $('#name').val();
         var body = $('#body').val();
 
+        // それぞれが空でなければ
         if (name != "" && body != "") {
+            // 画像取得のために、formDataオブジェクトを作成する
             var fd = new FormData();
+            // 以下の文でフォームデータを取得する
             var fd = new FormData($('#send-form').get(0));
 
+            // formDataオブジェクトにパラメータを追加する。'name'パラメータに変数nameを追加する
             fd.append('name', name);
             fd.append('body', body);
 
+            // Ajax経由で画像登録する
             $.ajax({
-                url: 'upload.php',
+                url: 'upload.php', // 画像登録を行うphpファイルを指定する
                 type: 'POST',
                 data: fd,
                 cache: false,
@@ -21,11 +27,11 @@ window.onload = function () {
                 dataType: 'html'
             }).done(function (data) {
                 alert("送信が完了しました");
-                location.href = "./";
+                location.href = "./"; // ページをリロードする
             });
         } else {
             alert("未入力の項目があります");
         }
-        return false;
+        return false; // submit をキャンセルする
     });
 }
--- upload.php
+++ upload.php
@@ -1,15 +1,21 @@
 <?php
+// POSTデータの name と body をバリデーションにかける
 if(!empty($_POST['name']) && !empty($_POST['body'])){
+    // imageタイプを識別
     $imginfo = getimagesize($_FILES['file1']['tmp_name']);
     if($imginfo['mime'] == 'image/jpeg'){$extension = ".jpg";}
     if($imginfo['mime'] == 'image/png'){$extension = ".png";}
     if($imginfo['mime'] == 'image/gif'){$extension = ".gif";}
 
+    // 拡張子が入っているなら
     if(!empty($extension)){
-
+        // アップロードされたファイル名を取得する
         $file_tmp = $_FILES['file1']['tmp_name'];
+        // ファイル名を決める
         $file_name = $_POST['name'] . $extension;
+        // ファイルの保存場所を決める
         $file_save = "./images/" . $file_name;
+        // $_FILES['file1']['tmp_name'] から $file_save の保存場所へ移動させる
         move_uploaded_file($file_tmp, $file_save);
 
         echo "success";
